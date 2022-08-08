10733e8015ba39b728357241f8d91ad4d8a40163
Yeseong <ohyeesung@naver.com>
2022-08-08 17:17:09 +0900

    2022 08 08

--- 나머지.py
+++ 나머지.py
@@ -1,8 +1,6 @@
-# data = [int(input()) for i in range(10)]
-# remain = []
-# for i in data:
-#     remain.append(i%42)
-# remain = set(remain)
-# print(len(remain))
-
-open(0)
+data = [int(input()) for i in range(10)]
+remain = []
+for i in data:
+    remain.append(i%42)
+remain = set(remain)
+print(len(remain))
