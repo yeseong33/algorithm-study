71ed9dd9513ab81da3d7cbddb433edd9aaaa4ebc
Yeseong <ohyeesung@naver.com>
2022-08-20 02:21:52 +0900

    2022 08 19

--- 브루트 포스.py/체스판 다시 칠하기.py
+++ 브루트 포스.py/체스판 다시 칠하기.py
@@ -16,34 +16,34 @@ def check(b, n, m):
     row = n
     col = m
     count = []
-    for i in range(row-8): 
-        b = list(b[i:i+8])
-        for j in range(col-8): 
+    for i in range(row-7): 
+        t = list(b[i:i+8])
+        for j in range(col-7): 
             ches = []
-            for k in b:
+            for k in t:
                 ches.append(k[j:j+8])
-            
-            print(ches)
-            c = 0
-            cor = ches[0][0]
-            for i in range(8):
-                for j in range(8):
-                    if i % 2 == 0: 
-                        if j % 2 == 0:
-                            if ches[i][j] != cor:
-                                c += 1
-                        else:
-                            if ches[i][j] == cor:
-                                c += 1
-                    if i % 2 != 0:
-                        if j % 2 == 0:
-                            if ches[i][j] == cor:
-                                c += 1
-                        else:
-                            if ches[i][j] != cor:
-                                c += 1
-            count.append(c)
-    print(count)
+                
+            corl = [ches[7][7] ,ches[0][0],ches[0][7],ches[7][0]]
+            for i in corl:
+                cor = i
+                c = 0
+                for i in range(8):
+                    for j in range(8):
+                        if i % 2 == 0: 
+                            if j % 2 == 0:
+                                if ches[i][j] != cor:
+                                    c += 1
+                            else:
+                                if ches[i][j] == cor:
+                                    c += 1
+                        elif i % 2 != 0:
+                            if j % 2 == 0:
+                                if ches[i][j] == cor:
+                                    c += 1
+                            else:
+                                if ches[i][j] != cor:
+                                    c += 1
+                count.append(c)
     return min(count)
 
                         
