e5f72593dcaa5beab0016cc7a3fe72aa55aebfd1
Yeseong <ohyeesung@naver.com>
2022-08-30 20:49:14 +0900

    2022 08 30

--- 정수론 및 조합론/최소공배수.py
+++ 정수론 및 조합론/최소공배수.py
@@ -1,27 +1,20 @@
 import sys
 
+def EA(a, b):
+    
+    if a % b != 0:
+        return EA(b, a%b)
+    else:
+        return b
+
 n = int(sys.stdin.readline())
 
 
-
-a = 10
-b = 6
-
-if a < b:
-    a, b = b, a
-for i in range(a, a*b+1 , a):
-    for j in range(b, a*b+1, b):
-        if i == j:
-            print(i)
-            break
-        elif i < j:
-            break 
-    if i == j:
-        break               
-
-
-# for i in range(n):
-#     a, b = map(int, sys.stdin.readline().split())
-#     print(max_co(a, b))
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    if a < b:
+        a, b= b, a
+    print(int(a * b /EA(a, b)))
     
 
+## 유클리드 호제법 -> 최소공배수, 최대공약수 구하는 식****
