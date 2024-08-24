0d56f8ba0a72ee05770a61077411c605373544d1
yeseong33 <ohyeesung@naver.com>
2024-08-24 17:50:09 +0900

    쇠막대기 : S1 : 20MIN : BOJ

---
+++ java/스택활용/쇠막대기.java
@@ -0,0 +1,58 @@
+package 스택활용;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class 쇠막대기 {
+
+    String req;
+    int steal, total;
+    Stack<Character> que;
+
+    void makeSettings() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        req = br.readLine();
+        steal = 0;
+        total = 0;
+        que = new Stack<>();
+    }
+
+    void slice() {
+        boolean isR = false;
+        for (char c : req.toCharArray()) {
+            if (c == '(') {
+                isR = true;
+                que.add(c);
+                steal += 1;
+                continue;
+            }
+            if (!que.isEmpty() && c == ')' && que.peek() == '(') {
+                if (isR) {
+                    isR = false;
+                    steal -= 1;
+                    total += steal;
+                } else {
+                    total +=1;
+                    steal -= 1;
+                }
+                que.pop();
+            }
+        }
+        System.out.println(total);
+    }
+
+    void solve() throws IOException {
+        makeSettings();
+        slice();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        new 쇠막대기().solve();
+
+    }
+}
