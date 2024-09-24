ecf7319d7bfd973c199efa1112800d339a50f552
yeseong33 <ohyeesung@naver.com>
2024-09-24 09:11:17 +0900

    N과 M(11) : S2 : 5MIN : BOJ

---
+++ java/백트레킹/N과M11.java
@@ -0,0 +1,84 @@
+package 백트레킹;
+
+import java.io.*;
+import java.util.*;
+
+public class N과M11 {
+
+
+    int n, m;
+    int[] visited, intValues;
+    BufferedWriter bw;
+    Stack<Integer> que;
+    StringTokenizer st;
+    String[] values;
+    Set<Integer> duplicate;
+
+    boolean makeSettings() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        values = br.readLine().split(" ");
+        intValues = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            intValues[i] = Integer.parseInt(values[i]);
+        }
+        Arrays.sort(intValues);
+
+
+        que = new Stack<>();
+        visited = new int[1000001];
+        return false;
+    }
+
+
+    boolean isFull() {
+        if (que.size() == m) {
+            return true;
+        }
+        return false;
+    }
+
+
+    void BT(int start) throws IOException {
+        if (isFull()) {
+            for (int value : que) {
+                bw.write(value+ " ");
+            }
+            bw.newLine();
+            return;
+        }
+        int before = 0;
+        for (int i = 0; i < n; i++) {
+//            if (visited[i] == 0) {
+                if (before != intValues[i]) {
+                    visited[i] += 1;
+                    que.add(intValues[i]);
+                    before = intValues[i];
+                    BT(i+1);
+                    que.pop();
+                    visited[i] -= 1;
+                }
+//            }
+        }
+    }
+
+
+    void solve() throws IOException {
+        if (makeSettings()) {
+            return;
+        }
+        BT(0);
+        bw.flush();
+        bw.close();
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        new N과M11().solve();
+    }
+}
+
+
